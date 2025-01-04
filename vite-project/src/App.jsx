d4eda7f4f87f1e1f920208d63fd8c9f5685143c8
[Diganta Sonowal] <digantas940@gmail.com>
2025-01-04 08:29:12 +0530

    created customReact folder and few changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,13 @@
 import Food from "./food" 
 
 function App() {
-  
+
+  const username = "Mark"
 
   return (
     
     <>
-      <h1>Vite React App</h1>
+      <h1>Vite React App {username}</h1>
       <h1>Vite App</h1>
       <Food />
 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,8 +1,38 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+const reactElement = {
+  type: 'a',
+  props: {
+      href: "https://google.com",
+      target: "_blank"
+  },
+  children: 'CLick me to visit google'
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  )
+}
+
+const AnotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google Website</a>
+)
+
+const areactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click to visit google'
+  
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <App/>
+  <App />
+
 )
